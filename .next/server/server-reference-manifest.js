9fc97d31ff0ab841ed48cc3b778696ee7eea200e
Dinuka Ashan <dinukaaw.sh@gmail.com>
2025-07-06 00:59:54 +0530

    fixed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7DTsDNyI8IktxdMcpF9WoaRWL5aGlPUdUONvwwVaaSw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_page_tsx_94424cb9._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_7d7a5143._.js",
-  "static/chunks/node_modules_next_dist_compiled_react_26ec58f1._.js",
-  "static/chunks/src_app_styles_0813ced7._.css"
-],
-    source: "dynamic"
-});
